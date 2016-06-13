4bf8724ced26e5f646467ea59c10026f1708a0b7
rhargreaves <kiyouta@gmail.com>
2016-06-13 21:29:35 +0100

    Fix script usage condition

--- test-vcl.sh
+++ test-vcl.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 VTC_FILE=$1
 VCL_FILE=$2
-COMBINED_FILE=/tmp/$(basename $VTC_FILE)
 
-if [ -e $VTC_FILE ] then
+cd /work
+if [ ! -e $VTC_FILE ]; then
 	echo "Usage: $(basename $0) <vtc_file> <vcl_file>"
 	exit 1
 fi
 
-cd /work
+echo "Testing $VTC_FILE with $VCL_FILE"
+COMBINED_FILE=/tmp/$(basename $VTC_FILE)
 sed -e '/# VCL_PLACEHOLDER/ r '${VCL_FILE} -e 's/# VCL_PLACEHOLDER//' ${VTC_FILE} > ${COMBINED_FILE}
 varnishtest ${COMBINED_FILE}
-
